32f46010f24f9d8d67598d50a4974e6d26b46960
Daniel Corn <d@cundd.net>
2015-02-06 11:59:30 +0100

    Increased the maximum depth to scan for changes

--- Classes/Command/CompileCommandController.php
+++ Classes/Command/CompileCommandController.php
@@ -237,7 +237,7 @@ class CompileCommandController extends CommandController {
 	 *
 	 * @var int
 	 */
-	protected $findFilesMaxDepth = 6;
+	protected $findFilesMaxDepth = 7;
 
 	/**
 	 * Array of watched files
