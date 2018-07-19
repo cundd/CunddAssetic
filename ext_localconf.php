93f18319b0b82553d13664b9373e4e2053708645
Daniel Corn <info@cundd.net>
2018-07-19 21:41:44 +0200

    Update the icons and add Composer JSON file

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,12 @@
 <?php
 
-use Cundd\Assetic\Command\AsseticCommandController;
+defined('TYPO3_MODE') || die('Access denied.');
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['assetic_cache'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['assetic_cache'] = [];
-}
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = AsseticCommandController::class;
+call_user_func(
+    function () {
+        if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['assetic_cache'])) {
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['assetic_cache'] = [];
+        }
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \Cundd\Assetic\Command\AsseticCommandController::class;
+    }
+);
--- ext_tables.php
+++ ext_tables.php
@@ -2,25 +2,32 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+call_user_func(
+    function () {
+        if (TYPO3_MODE === 'BE') {
+            /*
+             * Registers a Backend Module
+             */
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'Cundd.Assetic',
+                'web', // Make module a submodule of 'web'
+                'cunddassetic', // Submodule key
+                '', // Position
+                [
+                    'Asset' => 'list, compile',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon'   => 'EXT:assetic/Resources/Public/Icons/logo_icon-white.svg',
+                    'labels' => 'LLL:EXT:assetic/Resources/Private/Language/locallang_cunddassetic.xlf',
+                ]
+            );
+        }
 
-if (TYPO3_MODE === 'BE') {
-    /*
-     * Registers a Backend Module
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Cundd.' . $_EXTKEY,
-        'web', // Make module a submodule of 'web'
-        'cunddassetic', // Submodule key
-        '', // Position
-        [
-            'Asset' => 'list, compile',
-        ],
-        [
-            'access' => 'user,group',
-            'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.png',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_cunddassetic.xlf',
-        ]
-    );
-}
-
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Cundd Assetic');
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'assetic',
+            'Configuration/TypoScript',
+            'Cundd Assetic'
+        );
+    }
+);
