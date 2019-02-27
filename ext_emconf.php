8dc368c732c58d168d04464832ca3bbbecff3691
Daniel Corn <cod@iresults.li>
2019-02-27 14:55:20 +0100

    Basic support for TYPO3 9 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '1.5.1-dev',
     'constraints'      => [
         'depends'   => [
-            'typo3'          => '6.2.0-8.7.99',
+            'typo3'          => '6.2.0-9.5.99',
             'cundd_composer' => '3.0-4.99',
         ],
         'conflicts' => [
