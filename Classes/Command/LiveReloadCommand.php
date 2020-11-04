d2639ace26daf66c284a6de8a00fdc71e753c027
Daniel Corn <cod@iresults.li>
2020-11-04 17:46:52 +0100

    Add support for TLS secured websockets

--- Classes/Command/LiveReloadCommand.php
+++ Classes/Command/LiveReloadCommand.php
@@ -6,16 +6,23 @@ namespace Cundd\Assetic\Command;
 use Cundd\Assetic\FileWatcher\FileCategories;
 use Cundd\Assetic\Server\LiveReload;
 use Cundd\CunddComposer\Autoloader;
+use InvalidArgumentException;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Factory;
+use React\Socket\SecureServer;
+use React\Socket\Server;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use function array_merge;
 use function explode;
+use function file_exists;
 use function in_array;
+use function is_readable;
 use function pathinfo;
+use function sprintf;
 use const PATHINFO_EXTENSION;
 use const PHP_VERSION;
 
@@ -81,6 +88,18 @@ class LiveReloadCommand extends AbstractCommand implements ColorInterface
                 InputOption::VALUE_REQUIRED,
                 'Number of seconds to wait before sending the reload command to the clients',
                 0.0
+            )
+            ->addOption(
+                'tls-certificate',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Path to the TLS certificate in a PEM file (php.net/manual/context.ssl.php#context.ssl.local-cert)'
+            )
+            ->addOption(
+                'tls-private-key',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Path to the private key file (php.net/manual/context.ssl.php#context.ssl.local-pk)'
             );
     }
 
@@ -105,22 +124,39 @@ class LiveReloadCommand extends AbstractCommand implements ColorInterface
         }
         $this->printWatchedPaths($output);
 
-        // Websocket server
+        // LiveReload server
         $this->liveReloadServer = new LiveReload($notificationDelay);
-        $server = IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    $this->liveReloadServer
-                )
-            ),
-            $port,
-            $address
+
+        $component = new HttpServer(
+            new WsServer(
+                $this->liveReloadServer
+            )
         );
+
+        $useTLS = (bool)$input->getOption('tls-certificate');
+        if (!$useTLS) {
+            $server = IoServer::factory(
+                $component,
+                $port,
+                $address
+            );
+        } else {
+            $loop = Factory::create();
+
+            $server = new SecureServer(
+                new Server($address . ':' . $port, $loop),
+                $loop,
+                $this->buildSecureServerContext($input)
+            );
+
+            $server = new IoServer($component, $server, $loop);
+        }
 
         $server->loop->addPeriodicTimer($interval, [$this, 'recompileIfNeededAndInformLiveReloadServer']);
         $this->liveReloadServer->setEventLoop($server->loop);
+        $prefix = $useTLS ? 'Secure ' : '';
         $output->writeln(
-            "<info>Websocket server listening on $address:$port running under PHP version " . PHP_VERSION . "</info>"
+            "<info>{$prefix}Websocket server listening on $address:$port running under PHP version " . PHP_VERSION . "</info>"
         );
 
         $server->run();
@@ -146,7 +182,43 @@ class LiveReloadCommand extends AbstractCommand implements ColorInterface
             $this->liveReloadServer->fileDidChange($fileNeedsRecompile, false);
         } else {
             $changedFile = $this->compile(true);
-            $this->liveReloadServer->fileDidChange($changedFile);
+            $this->liveReloadServer->fileDidChange($changedFile, true);
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return array
+     */
+    private function buildSecureServerContext(InputInterface $input): array
+    {
+        return [
+            'local_cert'        => $this->assertTlsFilePath($input, 'tls-certificate'),
+            'local_pk'          => $this->assertTlsFilePath($input, 'tls-private-key'),
+            'allow_self_signed' => true,
+            'verify_peer'       => false,
+        ];
+    }
+
+    private function assertTlsFilePath(InputInterface $input, string $optionName): string
+    {
+        $path = $input->getOption($optionName);
+        if (null === $path) {
+            throw new InvalidArgumentException(sprintf('Option "%s" is not given', $optionName));
+        }
+
+        if (is_readable($path)) {
+            return $path;
+        }
+
+        if (file_exists($path)) {
+            throw new InvalidArgumentException(
+                sprintf('File "%s" for configuration %s exists, but is not readable', $path, $optionName)
+            );
+        } else {
+            throw new InvalidArgumentException(
+                sprintf('File "%s" for configuration %s does not exist', $path, $optionName)
+            );
         }
     }
 }
