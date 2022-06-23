320fffb9ffb227949ee0a57e30c85efacdf37c64
Daniel Corn <daniel@iresults.li>
2022-06-23 12:20:22 +0200

    Update configuration for TYPO3 v11

--- ext_tables.php
+++ ext_tables.php
@@ -3,25 +3,20 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 call_user_func(
-    function () {
-        if (TYPO3_MODE === 'BE') {
-            /*
-             * Registers a Backend Module
-             */
-            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                'Cundd.Assetic',
-                'web', // Make module a submodule of 'web'
-                'cunddassetic', // Submodule key
-                '', // Position
-                [
-                    'Asset' => 'list, compile',
-                ],
-                [
-                    'access' => 'user,group',
-                    'icon'   => 'EXT:assetic/Resources/Public/Icons/logo_icon-white.svg',
-                    'labels' => 'LLL:EXT:assetic/Resources/Private/Language/locallang_cunddassetic.xlf',
-                ]
-            );
-        }
+    static function () {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Assetic',
+            'web',
+            'cunddassetic',
+            '',
+            [
+                \Cundd\Assetic\Controller\AssetController::class => 'list, compile',
+            ],
+            [
+                'access' => 'user,group',
+                'icon'   => 'EXT:assetic/Resources/Public/Icons/logo_icon-white.svg',
+                'labels' => 'LLL:EXT:assetic/Resources/Private/Language/locallang_cunddassetic.xlf',
+            ]
+        );
     }
 );
