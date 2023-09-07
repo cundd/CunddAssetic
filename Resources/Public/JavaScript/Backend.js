5ce3cca06f01e2f4842a3b1062e4b45740628a42
Daniel Corn <daniel@iresults.li>
2023-09-07 18:06:20 +0200

    Fix the error message toggle in the backend

---
+++ Resources/Public/JavaScript/Backend.js
@@ -0,0 +1,22 @@
+(function () {
+    const button = document.getElementById('build-error-message-button')
+    const container = document.getElementById('build-error-message')
+
+    function show() {
+        container.hidden = false;
+        button.setAttribute('aria-expanded', 'true')
+    }
+
+    function hide() {
+        container.hidden = true;
+        button.setAttribute('aria-expanded', 'false')
+    }
+
+    button.addEventListener('click', () => {
+        if (container.hidden) {
+            show();
+        } else {
+            hide()
+        }
+    })
+}());
