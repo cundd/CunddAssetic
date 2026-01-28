623be490022e1a907bd273c02f40b7745414623b
Daniel Corn <daniel@iresults.li>
2026-01-28 21:01:30 +0100

    Add support to print compiled stylesheet to `STDOUT`

--- Classes/Command/CompileCommand.php
+++ Classes/Command/CompileCommand.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Cundd\Assetic\Command;
 
+use Cundd\Assetic\Configuration;
 use Cundd\Assetic\Configuration\ConfigurationFactory;
 use Cundd\Assetic\ManagerInterface;
+use Cundd\Assetic\ValueObject\FilePath;
+use Cundd\Assetic\ValueObject\Result;
+use Cundd\Assetic\ValueObject\Result\Err;
+use Cundd\Assetic\ValueObject\Result\Ok;
+use RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Site\SiteFinder;
@@ -66,25 +73,88 @@ class CompileCommand extends AbstractCommand
             throw $result->unwrapErr();
         }
 
-        $usedPath = $result->unwrap()->getAbsoluteUri();
-        if ($destination) {
-            $usedPath = $this->copyToDestination(
-                $configuration,
-                $usedPath,
-                $destination
-            );
-        }
-
-        $output->writeln(sprintf(
-            "Compiled assets and saved file to '%s' in %0.4fs",
-            $usedPath,
+        $path = $result->unwrap();
+        $result = $this->handleDestination(
+            $output,
+            $configuration,
+            $path,
+            $destination,
             $compileTime
-        ));
+        );
 
         if ($input->getOption('clear-page-cache')) {
             $this->cacheManager->flushCachesInGroup('pages');
         }
 
-        return self::SUCCESS;
+        return $result->isOk() ? self::SUCCESS : self::FAILURE;
+    }
+
+    /**
+     * @return Result<null,RuntimeException>
+     */
+    private function handleDestination(
+        OutputInterface $output,
+        Configuration $configuration,
+        FilePath $file,
+        string $destination,
+        float $compileTime,
+    ): Result {
+        if ('-' === $destination) {
+            return $this->writeToStdOut($output, $file, $compileTime);
+        }
+
+        if ($destination) {
+            $finalPath = $this->copyToDestination(
+                $configuration,
+                $file->getAbsoluteUri(),
+                $destination
+            );
+        } else {
+            $finalPath = $file->getAbsoluteUri();
+        }
+
+        $output->writeln(sprintf(
+            "Compiled assets and saved file to '%s' in %0.4fs",
+            $finalPath,
+            $compileTime
+        ));
+
+        return new Ok(null);
+    }
+
+    /**
+     * @return Result<null,RuntimeException>
+     */
+    private function writeToStdOut(
+        OutputInterface $output,
+        FilePath $filePath,
+        float $compileTime,
+    ): Result {
+        $errorOutput = $output instanceof ConsoleOutputInterface
+            ? $output->getErrorOutput()
+            : null;
+
+        $absolutePath = $filePath->getAbsoluteUri();
+        $content = file_get_contents($absolutePath);
+        if (false === $content) {
+            $message = sprintf(
+                'Could not read the compiled file at "%s"',
+                $absolutePath
+            );
+            $errorOutput?->writeln($message);
+
+            return new Err(new RuntimeException($message));
+        }
+
+        $output->write(
+            $content,
+            options: OutputInterface::OUTPUT_RAW | OutputInterface::VERBOSITY_QUIET
+        );
+        $errorOutput?->writeln(sprintf(
+            'Compiled assets in %0.4fs',
+            $compileTime
+        ));
+
+        return new Ok(null);
     }
 }
